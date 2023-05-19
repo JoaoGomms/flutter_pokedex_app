a0f2d0af0336854c4b9e844311a20de72d672676
João Gomes <joaoguilherme102030@gmail.com>
2023-05-19 20:32:58 -0300

    create function to get color depending on pokemon type

--- lib/app/features/pokemon/models/pokemon_type.dart
+++ lib/app/features/pokemon/models/pokemon_type.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_app/app/theme/palette.dart';
+
 class PokemonType {
   final String name;
 
@@ -6,4 +9,27 @@ class PokemonType {
   factory PokemonType.fromJson(Map<String, dynamic> json) {
     return PokemonType(json['name']);
   }
+
+  static getTypeColor(PokemonType mainType) =>
+      {
+        'bug': Palette.bugType,
+        'dark': Palette.darkType,
+        'dragon': Palette.dragonType,
+        'electric': Palette.electricType,
+        'fairy': Palette.fairyType,
+        'fighting': Palette.fightingType,
+        'fire': Palette.fireType,
+        'flying': Palette.flyingType,
+        'ghost': Palette.ghostType,
+        'normal': Palette.normalType,
+        'grass': Palette.grassType,
+        'ground': Palette.groundType,
+        'ice': Palette.iceType,
+        'poison': Palette.poisonType,
+        'psychic': Palette.psychicType,
+        'rock': Palette.rockType,
+        'steel': Palette.steelType,
+        'water': Palette.waterType,
+      }[mainType.name] ??
+      Colors.white;
 }
