0a706946df44d32278d0fab3dfaff94fc6f8912c
João Gomes <joaoguilherme102030@gmail.com>
2023-05-18 23:46:55 -0300

    add new routes

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_app/app/features/pokemon/presenter/views/home_page.dart';
 
+import 'features/pokemon/presenter/views/pokemon_detail_page.dart';
+import 'features/pokemon/presenter/views/splash_page.dart';
+
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {'/': (context) => const HomePage()},
+      routes: {
+        '/': (context) => SplashPage(),
+        '/home': (context) => const HomePage(),
+        '/details': (context) => const PokemonDetailPage(),
+      },
       initialRoute: '/',
     );
   }
