99a89d212fce8a57d4c9f2e97c99a7120ab04866
João Gomes <joaoguilherme102030@gmail.com>
2023-05-19 20:32:34 -0300

    update splash page with animated icon

--- lib/app/features/pokemon/presenter/views/splash_page.dart
+++ lib/app/features/pokemon/presenter/views/splash_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:pokedex_app/app/theme/icons/pokemon_app_icons.dart';
+import 'package:pokedex_app/app/theme/palette.dart';
 
 import '../controllers/home_controller.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   SplashPage({super.key});
 
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with SingleTickerProviderStateMixin {
   final HomeController _homeController = GetIt.I.get();
+  late AnimationController animationController;
+  late Animation<double> loadingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 4))..forward();
+    loadingAnimation = Tween<double>(begin: 0.0, end: 2.0).animate(animationController);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +30,17 @@ class SplashPage extends StatelessWidget {
 
     Future.delayed(const Duration(seconds: 4), () => Navigator.pushReplacementNamed(context, '/home'));
 
-    return const Scaffold(
-      backgroundColor: Colors.red,
+    return Scaffold(
+      backgroundColor: Palette.primary,
       body: Center(
-        child: CircularProgressIndicator(),
+        child: RotationTransition(
+          turns: loadingAnimation,
+          child: const Icon(
+            PokemonAppIcons.pokeball,
+            color: Palette.white,
+            size: 96,
+          ),
+        ),
       ),
     );
   }
