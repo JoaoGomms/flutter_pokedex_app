6bdfdbff73703b2199fdbd16dcd5d53966b557ab
João Gomes <joaoguilherme102030@gmail.com>
2023-05-19 20:31:13 -0300

    add assets and icons

---
+++ lib/app/theme/icons/pokemon_app_icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class PokemonAppIcons {
+  PokemonAppIcons._();
+
+  static const _kFontFam = 'PokemonApp';
+  static const String? _kFontPkg = null;
+
+  static const IconData pokeball = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rule = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData weight = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
