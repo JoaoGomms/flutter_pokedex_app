92aafa2bcb55609e4916a1d261a7c0975c6d7f11
João Gomes <joaoguilherme102030@gmail.com>
2023-05-20 19:34:56 -0300

    implement constrained box to component

--- lib/app/features/pokemon/presenter/views/components/pokemon_info_component.dart
+++ lib/app/features/pokemon/presenter/views/components/pokemon_info_component.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_app/app/features/pokemon/models/pokemon_move_model.dart';
+import 'package:pokedex_app/app/theme/icons/pokemon_app_icons.dart';
 
 class PokemonInfoComponent extends StatelessWidget {
   final num height;
@@ -9,37 +10,54 @@ class PokemonInfoComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: Text('About'),
-        ),
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Item(
-              value: '${weight.toString()} kg',
-              label: 'Weight',
-              icon: const Icon(Icons.person),
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width),
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: Text('About'),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Item(
+                    value: Text(
+                      '${weight.toString()} kg',
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                    label: 'Weight',
+                    icon: PokemonAppIcons.weight),
+                _separatorBuilder(),
+                Item(
+                  value: Text(
+                    '${height.toString()} m',
+                    style: Theme.of(context).textTheme.displayLarge,
+                  ),
+                  label: 'Height',
+                  icon: PokemonAppIcons.rule,
+                ),
+                _separatorBuilder(),
+                Item(
+                  value: Column(
+                    children: moves
+                        .map((e) => Text(
+                              e.name,
+                              style: Theme.of(context).textTheme.displayLarge,
+                            ))
+                        .toList()
+                        .sublist(0, 2),
+                  ),
+                  label: 'Moves',
+                )
+              ],
             ),
-            _separatorBuilder(),
-            Item(
-              value: '${weight.toString()} kg',
-              label: 'Height',
-              icon: const Icon(Icons.person),
-            ),
-            _separatorBuilder(),
-            Item(
-              value: '${weight.toString()} kg',
-              label: 'Moves',
-              icon: const Icon(Icons.person),
-            )
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -52,9 +70,9 @@ class PokemonInfoComponent extends StatelessWidget {
 
 class Item extends StatelessWidget {
   final String label;
-  final String value;
-  final Widget icon;
-  const Item({super.key, required this.label, required this.value, required this.icon});
+  final Widget value;
+  final IconData? icon;
+  const Item({super.key, required this.label, required this.value, this.icon});
 
   @override
   Widget build(BuildContext context) {
@@ -64,17 +82,25 @@ class Item extends StatelessWidget {
         children: [
           Row(
             children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: icon,
-              ),
-              Text(value)
+              icon != null
+                  ? Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Icon(
+                        icon,
+                        size: 14,
+                      ),
+                    )
+                  : Container(),
+              value
             ],
           ),
           const SizedBox(
             height: 12,
           ),
-          Text(label)
+          Text(
+            label,
+            style: Theme.of(context).textTheme.displayMedium,
+          )
         ],
       ),
     );
