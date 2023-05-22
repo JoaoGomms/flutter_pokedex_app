6a7edde64ecbdfb1a9b0ea3954ed3f1fcebca2d7
João Gomes <joaoguilherme102030@gmail.com>
2023-05-21 23:07:04 -0300

    create pokemon service test

--- test/app/features/pokemon/services/pokemon_service_test.dart
+++ test/app/features/pokemon/services/pokemon_service_test.dart
@@ -1,10 +1,59 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:pokedex_app/app/features/pokemon/models/pokemon_model.dart';
 import 'package:pokedex_app/app/features/pokemon/services/pokemon_service.dart';
 
+class DioMock extends Mock implements Dio {}
+
+const mockedPokeApiData = {
+  'id': 1,
+  'name': 'Pokemon Name',
+  'weight': 2,
+  'height': 2,
+  'sprites': {
+    'front_default': 'Sprite Front Default Url',
+  },
+  'moves': [
+    {
+      'move': {'name': 'Move Name'}
+    }
+  ],
+  'stats': [
+    {
+      'base_stat': 2,
+      'stat': {'name': 'Stat Name'}
+    }
+  ],
+  'types': [
+    {
+      'type': {'name': 'Pokemon Type'}
+    }
+  ],
+};
+
 void main() {
-  test('pokemon service ...', () async {
-    var service = PokemonService(Dio(BaseOptions(baseUrl: 'https://pokeapi.co/api/v2/')));
-    await service.fetchAllPokemons();
+  late Dio dio;
+  late PokemonService service;
+
+  setUp(() {
+    GetIt.I.registerFactory<Dio>(() => DioMock());
+    dio = GetIt.I.get<Dio>();
+
+    GetIt.I.registerFactory<PokemonService>(() => PokemonService(dio));
+    service = GetIt.I.get<PokemonService>();
+  });
+
+  test('Should return a List of Pokemons', () async {
+    var mockedPokeApiDataResponse =
+        Response(data: mockedPokeApiData, statusCode: 200, requestOptions: RequestOptions());
+    when(() => dio.get(any())).thenAnswer((_) async => mockedPokeApiDataResponse);
+
+    List<PokemonModel> pokemonsReceived = await service.fetchAllPokemons();
+
+    expect(service.limit, 21);
+    expect(pokemonsReceived, isA<List<PokemonModel>>());
+    expect(pokemonsReceived.length, 20);
   });
 }
